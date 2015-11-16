cdc98f8406034ec5ddc03b7a65ab12d847ea3242
Vaibhav Devpura <vaibhav.devpura@gmail.com>
2015-11-16 19:44:23 +0530

    pricer_full_data collection

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -39,11 +39,11 @@ object Main {
 
     val folders = dataFolder.listFiles().toList
 
-    val maps = getMaps
-    val a = maps._1
-    val b = maps._2
+//    val maps = getMaps
+//    val a = maps._1
+//    val b = maps._2
 
-    dumpToMongo(Json.toJson(a).toString(), Json.toJson(b).toString(), Json.toJson(cities.map(_.swap)).toString())
+//    dumpToMongo(Json.toJson(a).toString(), Json.toJson(b).toString(), Json.toJson(cities.map(_.swap)).toString())
 
 
 
@@ -68,7 +68,7 @@ object Main {
    }
 
   def process(filename: File): Unit = {
-    replaceString(filename, keyValues(filename), cities)
+//    replaceString(filename, keyValues(filename), cities)
     dumpPricerData(filename)
   }
 
@@ -138,83 +138,82 @@ object Main {
 
   case class Version(name: String, id: Int)
 
-  def getMaps: (scala.collection.mutable.Map[Make, Set[Model]], scala.collection.mutable.Map[Model, Set[Version]]) = {
-
-    val lines = Source.fromInputStream(getClass.getResourceAsStream("make-model-version.csv"), "UTF-8").getLines()
-
-    val makeModels = scala.collection.mutable.Map[Make, Set[Model]]()
-    val modelVersions = scala.collection.mutable.Map[Model, Set[Version]]()
-
-    def processLine(line: String): Unit = {
-      val dust = line.split("    ").map(_.trim)
-      val makes = dust(1)
-      val mDust = makes.split(",").map(_.trim)
-      val make = mDust(0)
-      val makeId = mDust(1)
-      val models = dust(2)
-      val moDust = models.split(",").map(_.trim)
-      val model = moDust(0)
-      val modelId = moDust(1)
-      val versions = dust(3)
-      val vDust = versions.split(",").map(_.trim)
-      val version = vDust(0)
-      val versionId = vDust(1)
-
-      val cMake = Make(make, makeId.toInt)
-      val cModel = Model(model, modelId.toInt)
-      val cVersion = Version(version, versionId.toInt)
-
-      if (makeModels contains cMake) {
-        makeModels(cMake) = makeModels(cMake) + cModel
-      } else {
-        makeModels += (cMake -> Set[Model](cModel))
-      }
-
-      if (modelVersions contains cModel) {
-        modelVersions(cModel) = modelVersions(cModel) + cVersion
-      } else {
-        modelVersions += (cModel -> Set[Version](cVersion))
-      }
-
-    }
-
-    lines.foreach(processLine(_))
-
-    (makeModels -> modelVersions)
-  }
-
-  def keyValues(filename: File): scala.collection.mutable.Map[String, Int] = {
-    val lines = Source.fromInputStream(getClass.getResourceAsStream("make-model-version.csv"), "UTF-8").getLines()
-
-    val map = scala.collection.mutable.Map[String, Int]()
-
-    def processLine(line: String): Unit = {
-      val dust = line.split("    ").map(_.trim)
-      val makes = dust(1)
-      val mDust = makes.split(",").map(_.trim)
-      val make = mDust(0)
-      val makeId = mDust(1)
-      val models = dust(2)
-      val moDust = models.split(",").map(_.trim)
-      val model =  moDust(0)
-      val modelId = moDust(1)
-      val versions = dust(3)
-      val vDust = versions.split(",").map(_.trim)
-      val version = vDust(0)
-      val versionId = vDust(1)
-
-      map += (make -> makeId.toInt)
-      map += (model -> modelId.toInt)
-      map += (version -> versionId.toInt)
-    }
-
-    lines.foreach(processLine(_))
-    map
-  }
+//  def getMaps: (scala.collection.mutable.Map[Make, Set[Model]], scala.collection.mutable.Map[Model, Set[Version]]) = {
+//
+//    val lines = Source.fromInputStream(getClass.getResourceAsStream("make-model-version.csv"), "UTF-8").getLines()
+//
+//    val makeModels = scala.collection.mutable.Map[Make, Set[Model]]()
+//    val modelVersions = scala.collection.mutable.Map[Model, Set[Version]]()
+//
+//    def processLine(line: String): Unit = {
+//      val dust = line.split("    ").map(_.trim)
+//      val makes = dust(1)
+//      val mDust = makes.split(",").map(_.trim)
+//      val make = mDust(0)
+//      val makeId = mDust(1)
+//      val models = dust(2)
+//      val moDust = models.split(",").map(_.trim)
+//      val model = moDust(0)
+//      val modelId = moDust(1)
+//      val versions = dust(3)
+//      val vDust = versions.split(",").map(_.trim)
+//      val version = vDust(0)
+//      val versionId = vDust(1)
+//
+//      val cMake = Make(make, makeId.toInt)
+//      val cModel = Model(model, modelId.toInt)
+//      val cVersion = Version(version, versionId.toInt)
+//
+//      if (makeModels contains cMake) {
+//        makeModels(cMake) = makeModels(cMake) + cModel
+//      } else {
+//        makeModels += (cMake -> Set[Model](cModel))
+//      }
+//
+//      if (modelVersions contains cModel) {
+//        modelVersions(cModel) = modelVersions(cModel) + cVersion
+//      } else {
+//        modelVersions += (cModel -> Set[Version](cVersion))
+//      }
+//
+//    }
+//
+//    lines.foreach(processLine(_))
+//
+//    (makeModels -> modelVersions)
+//  }
+//  def keyValues(filename: File): scala.collection.mutable.Map[String, Int] = {
+//    val lines = Source.fromInputStream(getClass.getResourceAsStream("make-model-version.csv"), "UTF-8").getLines()
+//
+//    val map = scala.collection.mutable.Map[String, Int]()
+//
+//    def processLine(line: String): Unit = {
+//      val dust = line.split("    ").map(_.trim)
+//      val makes = dust(1)
+//      val mDust = makes.split(",").map(_.trim)
+//      val make = mDust(0)
+//      val makeId = mDust(1)
+//      val models = dust(2)
+//      val moDust = models.split(",").map(_.trim)
+//      val model =  moDust(0)
+//      val modelId = moDust(1)
+//      val versions = dust(3)
+//      val vDust = versions.split(",").map(_.trim)
+//      val version = vDust(0)
+//      val versionId = vDust(1)
+//
+//      map += (make -> makeId.toInt)
+//      map += (model -> modelId.toInt)
+//      map += (version -> versionId.toInt)
+//    }
+//
+//    lines.foreach(processLine(_))
+//    map
+//  }
 
   def getTempFile(filename: File): File = {
     val folder = filename.getParentFile
-    new File(folder, folder.getName + "_processed.csv")
+    new File(folder, folder.getName + ".csv")
   }
 
   def getIdsFile(filename: File): File = {
@@ -238,7 +237,7 @@ object Main {
     val cols = line.split("    ").map(_.trim)
     val year = cols(0)
     val make = cols(1)
-    val model = if (cols(2) == "Land Cruiser") "Land Cruiser [2011-2015]" else cols(2)
+    val model = cols(2)
     val version = cols(3)
     val city = cols(4)
     val month = cols(5)
@@ -251,7 +250,7 @@ object Main {
 
     val fs = "    "
 
-    s"$year$fs${map(make)}$fs${map(model)}$fs${map(version)}$fs${cities(city)}$fs$month$fs$kms$fs$fair$fs$good$fs$excellent"
+    s"$year$fs$make$fs$model$fs$version$fs$city$fs$month$fs$kms$fs$fair$fs$good$fs$excellent"
   }
 
   def dumpToMongo(makesModels: String, modelsVersions: String, cities: String): Unit = {
@@ -261,15 +260,15 @@ object Main {
 
     val zoomo = mongoClient("zoomo")
 
-    (Json.parse(makesModels) \ "list").as[List[JsValue]].map { json =>
-      zoomo("pricer_make_model").insert(JSON.parse(json.toString).asInstanceOf[DBObject])
-    }
-    (Json.parse(modelsVersions) \ "list").as[List[JsValue]].map { json =>
-      zoomo("pricer_model_version").insert(JSON.parse(json.toString).asInstanceOf[DBObject])
-    }
-    (Json.parse(modelsVersions) \ "list").as[List[JsValue]].map { json =>
-      zoomo("pricer_cities").insert(JSON.parse(json.toString).asInstanceOf[DBObject])
-    }
+//    (Json.parse(makesModels) \ "list").as[List[JsValue]].map { json =>
+//      zoomo("pricer_make_model").insert(JSON.parse(json.toString).asInstanceOf[DBObject])
+//    }
+//    (Json.parse(modelsVersions) \ "list").as[List[JsValue]].map { json =>
+//      zoomo("pricer_model_version").insert(JSON.parse(json.toString).asInstanceOf[DBObject])
+//    }
+//    (Json.parse(modelsVersions) \ "list").as[List[JsValue]].map { json =>
+//      zoomo("pricer_cities").insert(JSON.parse(json.toString).asInstanceOf[DBObject])
+//    }
 
     mongoClient.close()
 
@@ -282,33 +281,34 @@ object Main {
     val mongoClient = MongoClient(host = "localhost", port = 27017)
     val zoomo = mongoClient("zoomo")
 
-    lines.foreach { line =>
-      val cols = line.split("    ").map(_.trim)
-      val year = cols(0).toInt
-      val make = cols(1).toInt
-      val model = cols(2).toInt
-      val version = cols(3).toInt
-      val city = cols(4).toInt
-      val month = cols(5)
-      val kms = cols(6).toInt
-      val fair = cols(7).toInt
-      val good = cols(8).toInt
-      val excellent = cols(9).toInt
-
-      val data = Json.obj(
-        "year" -> year,
-        "make" -> make,
-        "model" -> model,
-        "version" -> version,
-        "city" -> city,
-        "month" -> month,
-        "kms" -> kms,
-        "fair_price" -> fair,
-        "good_price" -> good,
-        "excellent_price" -> excellent
-      )
-      zoomo("pricer_data").insert(JSON.parse(data.toString).asInstanceOf[DBObject])
-    }
+    println(filename)
+//    lines.foreach { line =>
+//      val cols = line.split("    ").map(_.trim)
+//      val year = cols(0)
+//      val make = cols(1)
+//      val model = cols(2)
+//      val version = cols(3)
+//      val city = cols(4)
+//      val month = cols(5)
+//      val kms = cols(6).toInt
+//      val fair = cols(7).toInt
+//      val good = cols(8).toInt
+//      val excellent = cols(9).toInt
+//
+//      val data = Json.obj(
+//        "year" -> year,
+//        "make" -> make,
+//        "model" -> model,
+//        "version" -> version,
+//        "city" -> city,
+//        "month" -> month,
+//        "kms" -> kms,
+//        "fair_price" -> fair,
+//        "good_price" -> good,
+//        "excellent_price" -> excellent
+//      )
+//      zoomo("pricer_full_data").insert(JSON.parse(data.toString).asInstanceOf[DBObject])
+//    }
 
     mongoClient.close()
   }
